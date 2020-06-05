077ba8c695d99c4a6f2295370834c435062fcebe
Agnieszka <kuncika2000@gmail.com>
2020-06-05 18:26:03 +0200

    trial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          elo <code>src/App.tsx</code> and save to reload.
+          HEJKA <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
