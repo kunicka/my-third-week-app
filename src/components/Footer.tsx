7cbb4f9412a0f65d12919ebc2aceae0e1508109c
Agnieszka <kuncika2000@gmail.com>
2020-06-05 18:43:21 +0200

    scss template, header, footer, main template

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer:React.FC = () => {
+    return (
+        <div>
+            <h3>I am a sexi footer of this page</h3>
+        </div>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header:React.FC = () => {
+    return (
+        <div>
+            <h1>Dzień dobry!</h1>
+        </div>
+    )
+}
